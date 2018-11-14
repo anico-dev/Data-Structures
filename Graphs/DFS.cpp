edd3587b5b3a24ceb5d8ac598304faaf6498015b
Anirudh Mishra <35427320+anico-dev@users.noreply.github.com>
2018-11-14 23:14:24 +0530

    Add files via upload

---
+++ Graphs/DFS.cpp
@@ -0,0 +1,91 @@
+#include <iostream>
+#include <stack>
+#include <algorithm>
+using namespace std;
+class dfs
+{
+int *A;
+intarr[10][10];
+int v;
+
+public:
+dfs()
+    {
+inti,j;
+cout<<"enter no. of vertices: ";
+cin>>v;
+        A=new int[v];
+        for(i=1;i<=v;i++)
+        {
+            A[i]=0;
+        }
+
+        for(i=1;i<=v;i++)
+        {
+            for(j=1;j<=v;j++)
+            {
+arr[i][j]=0;
+            }
+        }
+
+    }
+
+    void create(inted)
+    {
+intx,y;
+for(inti=1;i<=ed;i++)
+        {
+cout<<"directed : ";
+cin>>x>>y;
+arr[x][y]=1;
+        }
+
+    }
+
+    void logic()
+    {
+        stack<int>s;
+int source;
+cout<<"enter source: ";
+cin>>source;
+
+        A[source]=1;
+s.push(source);
+int res;
+        while(!s.empty())
+        {
+        res=s.top();
+cout<<res<<" ";
+s.pop();
+
+for(inti=1;i<=v;i++)
+        {
+            if(arr[res][i]==1)
+            {
+                if(A[i]!=1)
+                {
+                    A[i]=1;
+s.push(i);
+                    break;//to exit the loop;
+                }
+            }
+        }
+        }
+    }
+
+
+
+}
+intmain()
+{
+
+dfs d;
+inted;
+cout<<"enter edges: ";
+cin>>ed;
+d.create(ed);
+d.logic();
+
+    return 0;
+}
+
